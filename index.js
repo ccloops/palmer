53025b9d4b85390f3b72e3095d2784c0aa15eff3
Catherine <loopercat@gmail.com>
2019-06-18 12:18:22 -0700

    scaffolding file structure, initializing and installing

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+import App from './src/app';
+import './src/styles/reset.css';
+import './src/styles/style.css';
+
+reactDOM.render(<App />, document.getElementById('root'));
